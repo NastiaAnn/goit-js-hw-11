63647729c50eae23fc774fbd8da6e0ee5a56e0ce
NastiaAnn <anastasianazar4@gmail.com>
2023-04-09 21:57:02 +0300

    9.04

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import { PixabayAPI } from './pixabay-api';
-import axios from 'axios';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const searchFormEl = document.querySelector('.search-form');
 const searchInputEl = document.querySelector('.form-input');
 const galleryEl = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
 
+let simpleLightBoxGallery;
+
 const pixabayAPI = new PixabayAPI();
 
-function handleFormSubmit(event) {
+async function handleFormSubmit(event) {
   event.preventDefault();
 
   if (searchInputEl.value === '') {
@@ -17,32 +21,55 @@ function handleFormSubmit(event) {
   pixabayAPI.query = searchInputEl.value.trim();
   searchInputEl.value = '';
 
-  pixabayAPI
-    .fetchPhotos()
-    .then(data => {
-      console.log(data);
-      galleryEl.innerHTML = renderGalleryMarkup(data);
+  try {
+    const { data } = await pixabayAPI.fetchPhotos();
+    if (data.totalHits === 0) {
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    }
+    Notify.success(`Hooray! We found ${data.totalHits} images`);
+    galleryEl.innerHTML = renderGalleryMarkup(data);
+    simpleLightBoxGallery = new SimpleLightbox('.gallery a', {
+      captions: true,
+      captionSelector: 'img',
+      captionType: 'attr',
+      captionsData: 'alt',
+      captionPosition: 'bottom',
+      captionDelay: 250,
+    });
 
-      if (data.totalHits <= pixabayAPI.count || data.totalHits === 0) {
-        loadMoreBtn.classList.add('is-hidden');
-        return;
-      }
-      loadMoreBtn.classList.remove('is-hidden');
-    })
-    .catch(err => console.log(err));
+    if (data.totalHits <= pixabayAPI.count || data.totalHits === 0) {
+      loadMoreBtn.classList.add('is-hidden');
+      return;
+    }
+    loadMoreBtn.classList.remove('is-hidden');
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-function handleLoadMoreBtnClick() {
+async function handleLoadMoreBtnClick() {
   pixabayAPI.page += 1;
-  pixabayAPI
-    .fetchPhotos()
-    .then(data => {
-      galleryEl.insertAdjacentHTML('beforeend', renderGalleryMarkup(data));
-      if (data.hits.length <= pixabayAPI.count) {
-        loadMoreBtn.classList.add('is-hidden');
-      }
-    })
-    .catch(err => console.log(err));
+  try {
+    const { data } = await pixabayAPI.fetchPhotos();
+    galleryEl.insertAdjacentHTML('beforeend', renderGalleryMarkup(data));
+    const lightBoxInstance = simpleLightBoxGallery.refresh();
+    lightBoxInstance.refresh();
+    if (data.hits.length <= pixabayAPI.count) {
+      loadMoreBtn.classList.add('is-hidden');
+      setTimeout(
+        () =>
+          Notify.warning(
+            "We're sorry, but you've reached the end of search results."
+          ),
+        3000
+      );
+    }
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 loadMoreBtn.addEventListener('click', handleLoadMoreBtnClick);
@@ -53,7 +80,9 @@ function renderGalleryMarkup({ hits }) {
     .map(hit => {
       return `
       <div class="photo-card">
-        <img src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy" height="427" />
+         <a class="gallery__item" href="${hit.largeImageURL}">
+            <img src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy" height="427" />
+        </a>
         <div class="info">
           <p class="info-item">
             <b>Likes </b>${hit.likes}
@@ -73,3 +102,14 @@ function renderGalleryMarkup({ hits }) {
     })
     .join('');
 }
+
+// function addSimpleLightBoxEl() {
+//   return (gallery = new SimpleLightbox('.gallery a', {
+//     captions: true,
+//     captionSelector: 'img',
+//     captionType: 'attr',
+//     captionsData: 'alt',
+//     captionPosition: 'bottom',
+//     captionDelay: 250,
+//   }));
+// }
--- src/pixabay-api.js
+++ src/pixabay-api.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export class PixabayAPI {
   #API_KEY = '35232464-dd394eb40b88e49b2f7bb554e';
   #BASE_URL = 'https://pixabay.com';
@@ -6,26 +8,21 @@ export class PixabayAPI {
   page = 1;
   count = 40;
 
-  baseSearchParams = {
-    per_page: this.count,
-    key: this.#API_KEY,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: 'true',
-  };
-
-  fetchPhotos() {
-    const searchParams = new URLSearchParams({
-      q: this.query,
-      page: this.page,
-      ...this.baseSearchParams,
-    });
-    return fetch(`${this.#BASE_URL}/api/?${searchParams}`).then(res => {
-      if (!res.ok) {
-        throw new Error(response.status);
-      }
-
-      return res.json();
-    });
+  async fetchPhotos() {
+    try {
+      return axios.get(`${this.#BASE_URL}/api/`, {
+        params: {
+          q: this.query,
+          page: this.page,
+          per_page: this.count,
+          key: this.#API_KEY,
+          image_type: 'photo',
+          orientation: 'horizontal',
+          safesearch: 'true',
+        },
+      });
+    } catch (err) {
+      throw new Error(err.message);
+    }
   }
 }
